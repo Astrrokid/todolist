aaab412dcb51157aa144fb69806bbe14d66bc71f
Astrrokid <109769847+Astrrokid@users.noreply.github.com>
2023-02-12 22:33:19 +0100

    Add files via upload
    first commit

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+function TodoItem({todoItems}) {
+    function deleteTodo(id){
+        let index = todoItems.findIndex(todoItem => todoItem.id === id)
+        todoItems.splice(index, 1)
+        localStorage.setItem('todoItems', JSON.stringify(todoItems))
+        window.location.reload()
+    }
+
+    return (
+        <>
+        {}
+            {todoItems.map(todoItem => (
+                <div className='todo-item' key={todoItem.id}>
+                    <p>{todoItem.todoText}</p>
+                    <i className="ri-delete-bin-3-fill"onClick={() => deleteTodo(todoItem.id)}></i>
+                </div>
+            ))}
+        </>
+    );
+}
+
+export default TodoItem;
